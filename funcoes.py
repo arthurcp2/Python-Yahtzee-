301fb4ed0aa1d174aa97d02aacab21151068647c
arthur pedreira <arthurcp2@al.insper.edu.br>
2026-04-23 20:06:37 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,9 @@
+def calcula_pontos_sequencia_baixa (l):
+    if 1 in l and 2 in l and 3 in l and 4 in l:
+        return 15
+    elif 2 in l and 3 in l and 4 in l and 5 in l:
+        return 15
+    elif 3 in l and 4 in l and 5 in l and 6 in l:
+        return 15
+    else:
+        return 0
