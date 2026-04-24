f89788b741c29bc15d3ef343572925ae2e4b6d88
Tutsmc-mc <arthurmc2@al.insper.edu.br>
2026-04-24 09:26:43 -0300

    Create EX7_funcoes.py

---
+++ EX7_funcoes.py
@@ -0,0 +1,7 @@
+def calcula_pontos_sequencia_alta (l):
+    if 1 in l and 2 in l and 3 in l and 4 in l and 5 in l:
+        return 30
+    elif 2 in l and 3 in l and 4 in l and 5 in l and 6 in l:
+        return 30
+    else:
+        return 0
