674e573b0840ce85d52cc2a1a34a9d385a1f9911
Tutsmc-mc <arthurmc2@al.insper.edu.br>
2026-04-27 13:09:59 +0000

    Calcula pontos na regra simples

--- funcoes.py
+++ funcoes.py
@@ -18,3 +18,10 @@ def remover_dado(lrolados,lestoque,irem):
     lrolados.append(lestoque[irem])
     del lestoque[irem]
     return [lrolados,lestoque]
+#EX4
+def calcula_pontos_regra_simples(lnu):
+    dic={1:0,2:0,3:0,4:0,5:0,6:0}
+    for i in lnu:
+        if i in dic:
+            dic[i]+=i
+    return dic
