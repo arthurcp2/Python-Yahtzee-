408a4bb1249c298ea506ea1f03f15d7d818b2c02
Tutsmc-mc <arthurmc2@al.insper.edu.br>
2026-04-27 13:11:39 +0000

    Calcula pontos na regra avançada - Sequência baixa

--- funcoes.py
+++ funcoes.py
@@ -31,3 +31,13 @@ def calcula_pontos_soma (l):
     for i in l:
         som+=i
     return som
+#EX6
+def calcula_pontos_sequencia_baixa (l):
+    if 1 in l and 2 in l and 3 in l and 4 in l:
+        return 15
+    elif 2 in l and 3 in l and 4 in l and 5 in l:
+        return 15
+    elif 3 in l and 4 in l and 5 in l and 6 in l:
+        return 15
+    else:
+        return 0
