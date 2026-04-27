2de0535fda18eb689a847b67b3a994544c53c1e8
arthur pedreira <arthurcp2@al.insper.edu.br>
2026-04-27 16:45:24 -0300

    Delete EX2_funcoes.py

--- EX2_funcoes.py
+++
@@ -1,7 +0,0 @@
-def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
-    lista_dados = []
-    dados_no_estoque.append(dados_rolados[dado_para_guardar])
-    del dados_rolados[dado_para_guardar]
-    lista_dados.append(dados_rolados)
-    lista_dados.append(dados_no_estoque)
-    return lista_dados
