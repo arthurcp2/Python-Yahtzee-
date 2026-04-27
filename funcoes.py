af1e3e89662d2529ea28ae31254b08c4d6bfc3d6
Tutsmc-mc <arthurmc2@al.insper.edu.br>
2026-04-27 13:08:20 +0000

    Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,11 @@ def rolar_dados(n):
     for i in range(n):
         l.append(random.randint(1, 6))
     return l
+#EX2
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    lista_dados = []
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+    del dados_rolados[dado_para_guardar]
+    lista_dados.append(dados_rolados)
+    lista_dados.append(dados_no_estoque)
+    return lista_dados
