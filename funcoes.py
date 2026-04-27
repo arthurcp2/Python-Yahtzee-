84ee3675ff9ae358e5e9c5597e6e2d82b47a955c
Tutsmc-mc <arthurmc2@al.insper.edu.br>
2026-04-27 13:09:00 +0000

    Remover dado

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,8 @@ def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     lista_dados.append(dados_rolados)
     lista_dados.append(dados_no_estoque)
     return lista_dados
+#EX3
+def remover_dado(lrolados,lestoque,irem):
+    lrolados.append(lestoque[irem])
+    del lestoque[irem]
+    return [lrolados,lestoque]
